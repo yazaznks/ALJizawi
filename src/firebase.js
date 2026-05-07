b4432cbe3a7c6f849cfa5368fb929d844f0e0834
Yazan Shehab <yazanshehab@Yazans-MacBook-Pro.local>
2026-05-07 10:58:27 +0300

    firebase

--- src/firebase.js
+++ src/firebase.js
@@ -3,14 +3,13 @@ import { getFirestore } from 'firebase/firestore';
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID
+  apiKey: "AIzaSyA_xAwcdzqmTy0w3fvCho4AR_Kp3ZIFQyo",
+  authDomain: "aljizawi-6f92f.firebaseapp.com",
+  projectId: "aljizawi-6f92f",
+  storageBucket: "aljizawi-6f92f.firebasestorage.app",
+  messagingSenderId: "536048158604",
+  appId: "1:536048158604:web:7e4f95089a8949208bb8a9"
 };
-
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
